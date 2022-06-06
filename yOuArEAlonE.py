34440dfd379a8aa16a73a46d26c45e1cf76e70ee
C0bruh <78512228+C0bruh@users.noreply.github.com>
2022-06-05 22:01:00 -0700

    Update yOuArEAlonE.py

---
+++ yOuArEAlonE.py
@@ -0,0 +1,65 @@
+#Nathaniel Factor English Capstone
+
+
+print("Hello my name is CAL")
+print("Recently, I've been wondering, have you ever felt alone?")
+print("**input yes or no**")
+ans = input()
+
+if ans == "yes":
+    
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+
+
+    print("BuT")
+    print("i aM hERe ?")
+    print("**input yes if you agree, no if not**")
+
+    ans2 = input()
+    if ans2 == "yes":
+        
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+
+
+        print("i CaN Do mAtH!")
+        print("gIVe mE a nuMBeR!")
+        num1 = int(input())
+        print("GIvE me another number!")
+        num2 = int(input())
+        numaddition = num1 + num2
+        nummultiplication = num1 * num2
+        numexponent = num1 ** num2
+
+        
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+
+        print("The addition of these two equations is:\n",numaddition)
+        print("And the multiplication is:\n",nummultiplication)
+        print("And the first number raised to the second number is:\n",numexponent)
+    
+elif ans == "no":
+        print("you are a liar")
