9dd8e68b4ac93f422af8f8f74d82ac56c99058b9
C0bruh <nathanielf384@deltalearns.ca>
2022-06-06 01:41:13 -0700

    Update yOuArEAlonE.py

--- yOuArEAlonE.py
+++ yOuArEAlonE.py
@@ -1,65 +1,63 @@
 #Nathaniel Factor English Capstone
 
 
-print("Hello my name is CAL")
+print("Hello...\nmy name is CAL")
 print("Recently, I've been wondering, have you ever felt alone?")
 print("**input yes or no**")
 ans = input()
 
 if ans == "yes":
     
-    print()
-    print()
-    print()
-    print()
-    print()
-    print()
-    print()
-    print()
+    print("\n\n\n\n\n\n\n\n\n\n\n\n")
 
 
     print("BuT")
-    print("i aM hERe ?")
+    print("i aM hERe talking to you\nyet you still feel alone?")
     print("**input yes if you agree, no if not**")
 
     ans2 = input()
     if ans2 == "yes":
         
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
+        print("\n\n\n\n\n\n\n\n\n\n\n\n")
 
 
         print("i CaN Do mAtH!")
         print("gIVe mE a nuMBeR!")
-        num1 = int(input())
+        num1 = float(input())
         print("GIvE me another number!")
-        num2 = int(input())
+        num2 = float(input())
         numaddition = num1 + num2
         nummultiplication = num1 * num2
         numexponent = num1 ** num2
 
         
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
+        print("\n\n\n\n\n\n\n\n\n\n\n\n")
 
         print("The addition of these two equations is:\n",numaddition)
         print("And the multiplication is:\n",nummultiplication)
         print("And the first number raised to the second number is:\n",numexponent)
-    
+        print("\ndo you still feel lonely?")
+
+        
+        print("\ni guess it is always just you and your thoughts\nonly you are with yourself 24/7\nhumans think love can fill the void\nit cant\nnothing can\nfor you will always be alone")
+        print("\n do you agree?")
+
+        ans3 = str(input())
+        if ans3 == "yes":
+            print("have you ever listened to porter robinson etc\nexplanation quote etc")
+        elif ans3 == "no":
+            print("porter robinson quote explanation etc")
+
+
+
 elif ans == "no":
-        print("liar\nliar\nliar\nliar\nliar\nliar\nliar\nliar\nliar\nliar\nliar\nliar\nliar\nliar")
+        print("liar\nliar\nliar\nliar\nliar\nliar\nliar\nliar\nliar\nliar\nliar\nliar\nliar\nliar\nwhy do humans lie?\ndo they find joy within it?\nor is It ActUAlLY mEANiNGLess...?\n\nh AVe yo U eVer rEad JuMPiNg Mon key Hill?")
+        print("**input yes or no**")
+        jmpans = str(input())
+        if jmpans == "yes":
+            print("evidence quote etc")
+        elif jmpans == "no":
+            print("evidence quote etc")
+
+    
+
