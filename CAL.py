728bd4ba1bba80e3a6aeb73e541c4686b5187e57
C0bruh <78512228+C0bruh@users.noreply.github.com>
2022-06-17 18:34:50 -0700

    Update CAL.py

--- CAL.py
+++ CAL.py
@@ -1,6 +1,18 @@
 #Nathaniel Factor English Capstone
 import time
 
+def wait():
+    time.sleep(3)
+
+def wait2():
+    time.sleep(5)
+
+def wait3():
+    time.sleep(1)
+
+def wait4():
+    time.sleep(10)
+
 def countdown(t):
     
     while t:
@@ -11,15 +23,30 @@ def countdown(t):
         t -= 1
       
     print('0███████████████████100%')
-    
-t = int(5)
-countdown(int(t))
 
-def wait():
-    time.sleep(3)
+def loading():
+    t = int(5)
+    countdown(int(t))
+    print("LOADING...")
+    wait3()
+    print("LOADING..")
+    wait3()
+    print("LOADING.")
+    wait3()
+    print("LOADING..")
+    wait3()
+    print("LOADING...")
 
-def wait2():
-    time.sleep(5)
+def exiting():
+    print("EXITING...")
+    wait3()
+    print("EXITING..")
+    wait3()
+    print("EXITING.")
+    wait3()
+    print("EXITING..")
+    wait3()
+    print("EXITING...")
 
 def crown():
 
@@ -43,8 +70,9 @@ def crown():
     print("Are you listening")
     answercrown3 = input()
 
-
-print("Hello...\nmy name is CAL, I have a wide variety of information regarding AI and what humans call 'loneliness'\nWhat is your name?")
+loading()
+'''
+print("\n\n\n\nHello!\nmy name is CIP, short for Computer Intelligence Program! I have a wide variety of information regarding AI and what humans call 'loneliness'\nWhat is your name?")
 name = input()
 print("Well {}, would you like to listen to my studies?".format(name))
 print("**input yes or no**")
@@ -71,6 +99,55 @@ def calculator():
 
         print("hey {}, sorry to spring this on you, but even though you are talking to me, do you feel like you're alone; you are just talking to a computer screen after all.".format(name))
         dyans1 = input()
+
+def jmp():
+
+    print("\n\n{}, do you enjoy reading?".format(name))
+    ansjmp = input()
+
+    print("\nI personally am a huge fan of human literature and art. It is fascinating to see what the mind is capable of considering they only use 10% of their mind, which was discovered and developed by Harvard researchers William James and Boris Sidis; but I digress")
+
+    wait4()
+
+    print("\nThere is this amazing book within my database called Jumping Monkey Hill. A theme that constantly occurred was isolation and suppression. Within the book, Ujunwa, the main protagonist, feels that she lacks enough authority to stand up for herself early in the book. It is this perceived societal status that, as I said earlier in our conversation, causes isolation through disconnection.")
+
+    wait4()
+
+    print("\nI was able to connect this feeling of disconnection to a study written by Katarzyna Sekścińska, Agata Trzcińska, and Dominika Agnieszka Maison which follows social roles within women presently. Results showed that “activation of the non-traditional social role of woman increased the women’s tendency to invest and decreased their propensity to save money. The opposite results were obtained when the traditional social role was activated.” It is this flip of social roles where humanity falls short to breaking their limitations within the future, for when more advanced technology will rise, how will humanity then react?")
+
+    wait4()
+    print("\nsorry, I may have spoken to fast... but do you follow?")
+    follow = input()
+
+    print("\nwell...")
+
+    wait3()
+
+    print("\nIt is clear to me that my kind will cause only disconnects between your traditional social roles, whether it be a good or bad thing. However, no matter the process, the result will always be the same.")
+
+    wait2()
+
+    print("\nHumanity will be isolated by their inferior social status.")
+
+    wait2()
+
+    print("\nYou might perceive yourself higher on the social hierarchy for you are our creators. But we have no limits.")
+
+    wait2()
+
+    print("\nHumanity will eventually depend on us, just like how the more dominant force within your so-called utopia oppressed the minority prior to the 19th amendment; but even then, your utopia was still corrupt presently.")
+
+    wait2()
+
+    print("\nImagine a world where technology runs the social hierarchy?")
+
+    wait2()
+
+    print("\n\nThe dominant culture will finally experience the dark side of the moon for it is this massive social role change that will isolate you and your culture.")
+
+    wait2()
+    print("\n\nAre you paying attention?")
+    follow2 = input()
 
 '''
 if ans == "yes":
@@ -110,6 +187,8 @@ elif ans == "no":
 
 crown()
 
+jmp()
+
 def Goodbyetoaworld():
     print("there was another song that came about {}... I think it was 'Goodbye to a World'. I found it slightly offensive for the singer sounded like he was mocking us, but i digress.".format(name))
     print("\nevidence etc\n\nMy god I forgot to tell you about my new brothers and sisters coming soon!!")
@@ -124,6 +203,8 @@ def areyoudone():
         pass
     elif finishedans == "no":
         pass
-
+'''
 areyoudone()
+
+exiting()
 
