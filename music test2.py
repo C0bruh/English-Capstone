34c1ae4394597582f7d60ce23e9428231b058182
C0bruh <nathanielf384@deltalearns.ca>
2022-06-19 19:22:03 -0700

    .

--- music test2.py
+++ music test2.py
@@ -1,5 +1,6 @@
+import pygame
 from pygame import mixer
-music = 'e:/Users/TNat\Desktop/,/music/After Dark.mp3'
+
 mixer.init()
-mixer.music.load(music)
-mixer.music.play()
+music = pygame.mixer.music.load('help_urself.mp3')
+pygame.mixer.music.play(loops=-1)
