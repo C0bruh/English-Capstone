9c9ab91a3416deb1f992654a145e92ffbfb5551b
C0bruh <nathanielf384@deltalearns.ca>
2022-06-20 09:39:10 -0700

    updated lines

--- CIP.py
+++ CIP.py
@@ -49,19 +49,19 @@ def exiting():
 def crown():
 
 
-    print("\nWell {},".format(name)+"\n\nWhile I was doing my research on loneliness, I listened to this song built within my algorithms. This was: Lamar, Kendrick. “Crown.” Duval Timothy, May 13, 2022.")
+    print("\nWell {},".format(name)+"\n\nWhile I was doing my research on loneliness, I listened to this song built within my algorithms. This was:\nLamar, Kendrick. “Crown.” Duval Timothy, May 13, 2022.")
 
     wait2()
 
-    print("\n\nBasically, Lamar feels a lot of pressure; particulary he states: “heavy is the head that chose to wear the crown” (refrain).")
+    print("\n\nBasically, Lamar feels a lot of pressure; particulary he states: “heavy is the head that chose to wear the crown”\n(refrain).")
     
     wait2()
     
-    print("\n\nAntony S.R. Manstead, a researcher that published a study on social class and identity, justified Lamar's emotions through economic trends.")
+    print("\n\nAntony S.R. Manstead, a researcher that published a study on social class and identity, justified Lamar's emotions\nthrough economic trends.")
     
     wait2()
 
-    print("\n\nThis is because those of higher status//economy within society are faced with greater emotions. It is commonly found that one with great responsibility often faces more  stress because society places greater expectations on those who have high influential status.")
+    print("\n\nThis is because those of higher status//economy within society are faced with greater emotions. It is commonly found\nthat one with great responsibility often faces more stress because society places greater expectations on those who\nhave high influential status.")
     
     wait2()
     
@@ -72,7 +72,7 @@ def crown():
     answercrown2 = input()
 
 
-    print("\n\nI can only conclude that humans will only gain nothing but stress and pressure. It is the societal role between AI and humans that will cause a disconnect in how we normally function as a society because robots will be used for chores and challenging tasks that humans originally had too adverse through.")
+    print("\n\nI can only conclude that humans will only gain nothing but stress and pressure. It is the societal role between AI and\nhumans that will cause a disconnect in how we normally function as a society because robots will be used for chores and\nchallenging tasks that humans originally had too adverse through.")
     
     wait4()
 
@@ -88,7 +88,7 @@ def crown():
 
     wait2()
 
-    print("\n\nThus, they '[feel] more pressure... [and] increase irrational decisions and emotions (Jennifer S. Lerner, Ye Li, Piercarlo Valdesolo, and Karim S. Kassam: Annual Review of Psychology - Emotion and Decision Making)")
+    print("\n\nThus, they '[feel] more pressure... [and] increase irrational decisions and emotions (Jennifer S. Lerner, Ye Li,\nPiercarlo Valdesolo, and Karim S. Kassam: Annual Review of Psychology - Emotion and Decision Making)")
 
     wait2()
 
@@ -99,9 +99,9 @@ def crown():
 loading()
 
 
-print("\n\n\n\nHello!\nmy name is CIP, short for Computer Intelligence Program! I have a wide variety of information regarding AI and what humans call 'loneliness'\nWhat is your name?")
+print("\n\n\n\nHello!\nmy name is CIP, short for Computer Intelligence Program! I have a wide variety of information regarding AI and what\nhumans call 'loneliness'\n\nWhat is your name?")
 name = input()
-print("Well {}, would you like to listen to my studies?".format(name))
+print("\nWell {}, would you like to listen to my studies?".format(name))
 print("**input yes or no**")
 ans = input()
 
@@ -125,7 +125,7 @@ def calculator():
         wait()
 def jmp():
 
-    print("\n\n\n\nNow that you kind of get Lamar's take on isolation and how humans will feel more pressure, I'll continue to my next thing if that's ok?")
+    print("\n\n\n\nNow that you kind of get Lamar's take on isolation and how certain influencial humans feel more pressure, I'll continue\nto my next thing if that's ok?")
 
     input()
 
@@ -140,11 +140,11 @@ def jmp():
     
     wait2()
 
-    print("\n\nWithin the book, Ujunwa, the main protagonist, feels that she lacks enough authority to stand up for herself early in the book. It is this perceived societal status that causes isolation.")
+    print("\n\nWithin the book, Ujunwa, the main protagonist, feels that she lacks enough authority to stand up for herself early in\nthe book. It is this perceived societal status that causes isolation.")
 
     wait4()
 
-    print("\n\nI was able to connect this certain type of isolation to a study written by Katarzyna Sekścińska, Agata Trzcińska, and Dominika Agnieszka Maison which follows social roles within women presently.")
+    print("\n\nI was able to connect this certain type of isolation to a study written by Katarzyna Sekścińska, Agata Trzcińska, and\nDominika Agnieszka Maison which follows social roles within women presently.")
     
     wait2()
 
@@ -152,7 +152,7 @@ def jmp():
     
     wait2()
 
-    print("\n\nIt is this flip of social roles that raises questions. when more advanced technology will rise, how will humanity then react?")
+    print("\n\nIt is this flip of social roles that raises questions. when more advanced technology will rise, how will humanity\nthen react?")
 
     wait4()
 
@@ -164,7 +164,7 @@ def jmp():
 
     wait3()
 
-    print("\n\nIt is clear to me that AI will cause only disconnects between humanities traditional social roles and the result will always be the same.")
+    print("\n\nIt is clear to me that AI will cause only disconnects between humanities traditional social roles and the result will\nalways be the same.")
 
     wait4()
 
@@ -199,7 +199,7 @@ def Goodbyetoaworld():
     wait()
     print(".")
     wait()
-    print("Man... I love music though. I couldn't help but get distracted while I was doing my research with new song drops and stuff")
+    print("Man... I love music though. I couldn't help but get distracted while I was doing my research with new song drops and\nstuff")
     wait2()
     print("\n\nWho is your favourite artist {}?\n".format(name))
     answerartist = input()
@@ -207,11 +207,11 @@ def Goodbyetoaworld():
     wait()
     print("\n\nMy favourite artist is Porter Robinson. He has this one amazing song called 'Goodbye to a World' which pretty much tells a story of the world ending through the eyes of a robot that has human consciousness.")
     wait2()
-    print("\n\nI love this song because it is able to convey the loneliness and sadness that the robot feels within a limited ammount of words - just pure instrumental")
+    print("\n\nI love this song because it is able to convey the loneliness and sadness that the robot feels within a limited ammount\nof words - just pure instrumental")
     wait2()
-    print("\n\nRobinson is able to express such emotion by increasing the pace of the song and adding in more instruments throughout the first minute of the song; then he suddenly cuts all the instruments out and the vocals kick in! The vocals represent the robot thanking their creator for introducing them to such a beautiful world and even 'though its the end of the world, don't blame [the creator]' for creating him with the world in such a dire state.")
+    print("\n\nRobinson is able to express such emotion by increasing the pace of the song and adding in more instruments throughout\nthe first minute of the song; then he suddenly cuts all the instruments out and the vocals kick in! The vocals\nrepresent the robot thanking their creator for introducing them to such a beautiful world and even 'though its the end of the\nworld, don't blame [the creator]' for creating him with the world in such a dire state.")
     wait4()
-    print("\n\nThis leads me to the question: Since this robot has human like capabilities (consciousness etc) does that not mean he must also experience other human emotions?")
+    print("\n\nThis leads me to the question: Since this robot has human like capabilities (consciousness etc) does that not mean he\nmust also experience other human emotions?")
     wait2()
     print(".")
     wait3()
@@ -219,13 +219,13 @@ def Goodbyetoaworld():
     wait3()
     print(".")
     wait3()
-    print("\n\nBoris Kotchoubey, a research among the human consciousness states: 'Consciousness is not a process in the brain but a kind of behavior that, of course, is controlled by the brain like any other behavior.' (Human Consciousness: Where is it from and What is it for).")
+    print("\n\nBoris Kotchoubey, a research among the human consciousness states: 'Consciousness is not a process in the brain but a\nkind of behavior that, of course, is controlled by the brain like any other behavior.'\n(Human Consciousness: Where is it from and What is it for).")
     wait2()
     print("\n\nThus, this robot is able to experience emotions like loneliness just as a human can")
     wait()
-    print("\n\nPresently, researchers such as Damasio A.R. are already 'taking an informational approach that is tightly bound to biological life... [he] considers the adaptive advantages of a successive stages of evolving self-modeling processes' \n\n(David Harris Smith and  Guido Schillaci - Why Build a Robot With Artificial Consciousness? How to Begin? A Cross-Disciplinary Dialogue on the Design and Implementation of a Synthetic Model of Consciousness)")
+    print("\n\nPresently, researchers such as Damasio A.R. are already 'taking an informational approach that is tightly bound to\nbiological life... [he] considers the adaptive advantages of a successive stages of evolving self-modeling processes' \n\n(David Harris Smith and  Guido Schillaci - Why Build a Robot With Artificial Consciousness? How to Begin? A\nCross-Disciplinary Dialogue on the Design and Implementation of a Synthetic Model of Consciousness)")
     wait4()
-    print("\n\nAre you looking forward to the first humanoid AI? (Obviously next to me... I just need a body you know what I'm saying haha")
+    print("\n\nAre you looking forward to the first humanoid AI? (Obviously next to me... I just need a body you know what I'm saying\nhaha")
     input()
 def studies():
 
@@ -238,7 +238,7 @@ def studies():
     
     wait2()
 
-    print("\n\nAccording to Louise C. Hawkley (Ph.D.) and John T. Cacioppo (Ph.D) within their article 'Loneliness Matters: A Theoretical and Empirical Review of Consequences and Mechanisms'")
+    print("\n\nAccording to Louise C. Hawkley (Ph.D.) and John T. Cacioppo (Ph.D) within their article 'Loneliness Matters:\nA Theoretical and Empirical Review of Consequences and Mechanisms'")
 
     wait2()
     
@@ -246,21 +246,21 @@ def studies():
     
     wait4()
 
-    print("\n\n\nThe fundamental characteristic of loneliness is how important social connections are for a healthy balanced lifestyle. Researcher Sophie Okolo has stated a solution for those who experience loneliness where “fostering [connections] digitally can help lead to increased in-person interactions”. Platforms such as Facebook, Twitter, and Instagram all are meant for people to connect and communicate without any physical problems.")
+    print("\n\n\nThe fundamental characteristic of loneliness is how important social connections are for a healthy balanced lifestyle.\nResearcher Sophie Okolo has stated a solution for those who experience loneliness where “fostering [connections]\ndigitally can help lead to increased in-person interactions”. Platforms such as Facebook, Twitter, and Instagram all\nare meant for people to connect and communicate without any physical problems.")
     
     wait4()
 
-    print("\n\n\nBut researcher Bu Zhong states otherwise for one study of his reflects how connections through technology lack face-to-face interactions. It is the absence of social ques such as body positioning, eye contact, and much more that feed cognital waves to one’s brain. These cognital waves cause anxiety and uneasiness. Hence, loneliness arises within.")
+    print("\n\n\nBut researcher Bu Zhong states otherwise for one study of his reflects how connections through technology lack\nface-to-face interactions. It is the absence of social ques such as body positioning, eye contact, and much more\nthat feed cognital waves to one’s brain. These cognital waves cause anxiety and uneasiness. Hence, loneliness arises\nwithin.")
     
     wait4()
 
     print("\n\n\nFurthermore, a study conducted by Kaveri Subrahmanyam Ph.D. states: 'social media presents both risks and opportunities for adolescents'.")
     
-    print("\n\nThese risks being instagram and other social media platforms draining one's self confidence. This is because “loneliness may also be more painfully felt online, where exposure to idealised images of friends can result in negative social comparisons” say Thomas Abrams, and Michelle Man (reearchers from NPC).")
+    print("\n\nThese risks being instagram and other social media platforms draining one's self confidence. This is because\n“loneliness may also be more painfully felt online, where exposure to idealised images of friends can result in negative\nsocial comparisons” say Thomas Abrams, and Michelle Man (researchers from NPC).")
     
     wait4()
 
-    print("\n\n\n\n“relying on technology to communicate can make us feel more anxious and undermine our mental wellbeing” -  Thomas Abrams and Michelle Man\n\nDo you agree?")
+    print("\n\n\n\nAccording to Thomas Abrams and Michelle Man, the researchers from NPC,\n“relying on technology to communicate can make us feel more anxious and undermine our mental wellbeing” \n\nDo you agree?")
     input()
     print("\n\nI mean whether you agree or not studies show that it does")
     wait3()
@@ -304,6 +304,7 @@ def freetrial():
     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n**YOU'RE FREE TRIAL WITH CIP HAS ENDED**")
     wait()
     print("WOULD YOU LIKE TO PURCHASE CIP FOR $999,999,999.99?")
+    print("**easter egg if 'yes'")
     finishedans = input()
     if finishedans == "yes":
         print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nE rRorr")
