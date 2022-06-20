fbcedf9a19e6308c1c16108ea611ed9b68344cb6
C0bruh <nathanielf384@deltalearns.ca>
2022-06-20 00:08:44 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,3 +17,17 @@ setup(
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
 )
+from setuptools import setup
+
+APP = ['CIP.py']
+DATA_FILES = []
+OPTIONS = {
+ 'argv_emulation': True,
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
